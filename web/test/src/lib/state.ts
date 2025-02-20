e3511e14478a1d7db1359d2399dd798f7c6e36d1
wighawag <wighawag@gmail.com>
2025-02-20 09:16:45 +0000

    type

--- web/test/src/lib/state.ts
+++ web/test/src/lib/state.ts
@@ -1,1 +1,1 @@
-export const error = {type: 'criticalError', errors: [{message: 'error'}]};
+export const error: {type: 'criticalError', errors: {message: string}[]} = {type: 'criticalError', errors: [{message: 'error'}]};
